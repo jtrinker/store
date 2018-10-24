4e88393b9be3b5750213f998e167b598ee817e2d
Jamie Trinker <jamietrinker@Jamies-MBP.attlocal.net>
2018-10-24 18:44:38 -0400

    initial commit

---
+++ src/components/StorePicker.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+    render() {
+       return (
+        <React.Fragment>
+            <p>fish</p>
+            <form className="store-selector">
+                <h2>Please select a store</h2>
+            </form>
+        </React.Fragment>
+       )
+    }
+}
+
+export default StorePicker;
